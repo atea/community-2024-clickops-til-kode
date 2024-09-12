ad78f22d4b61ece7e04315b9b8fb81111fef52b2
Trond Jakob Sjøvang <trond.sjovang@atea.no>
2024-09-12 15:11:45 +0200

    more scaffolding

--- infra/azure-landing-zones/main.tf
+++ infra/azure-landing-zones/main.tf
@@ -1,1 +1,26 @@
 data "azurerm_client_config" "current" {}
+
+module "azure_landing_zones" {
+    source = "Azure/caf-enterprise-scale/azurerm"
+    version = "6.1.0"
+
+    default_location = "westeurope"
+
+    providers = {
+        azurerm = azurerm
+        azurerm.connectivity = azurerm.connectivity
+        azurerm.management = azurerm
+    }
+
+    root_parent_id = data.azurerm_client_config.current.tenant_id
+    root_id = var.root_id
+    root_name = var.root_name
+
+    deploy_identity_resources = var.deploy_identity_resources
+    subscription_id_identity = var.identity_subscription_id
+    configure_identity_resources = local.configure_identity_resources
+
+    deploy_management_resources    = var.deploy_management_resources
+    subscription_id_management = var.management_subscription_id
+    configure_management_resources = local.configure_management_resources
+}
--- infra/azure-landing-zones/terraform.tf
+++ infra/azure-landing-zones/terraform.tf
@@ -2,14 +2,26 @@ terraform {
     required_providers {
       azurerm = {
         source = "hashicorp/azurerm"
-        version = "~> 4.0"
+        version = "~> 3.0"
+        configuration_aliases = [
+          azurerm.connectivity
+        ]
       }
     }
     required_version = "~> 1.0"
 }
 
 provider "azurerm" {
-  resource_provider_registrations = "core"
   subscription_id = var.management_subscription_id
-  features {}
+  features {
+
+  }
+}
+
+provider "azurerm" {
+  alias = "connectivity"
+  subscription_id = var.connectivity_subscription_id
+  features {
+    
+  }
 }
